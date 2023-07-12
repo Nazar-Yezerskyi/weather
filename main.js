ae9b8682c7cd996049e8af2970a60097d193cc80
Назар Єзерський <nazar@Nazars-MacBook-Pro.local>
2023-07-12 14:59:30 +0300

    second #2
    correction of errors

--- main.js
+++ main.js
@@ -9,8 +9,35 @@ function initialize() {
   const buttonElem = document.querySelector('.modal__main button');
 
   function checkWeather(city) {
-    const apiKey = 'df59189ce5d7f9c060464802b2848b6f';
-    const apiUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
+    fetch(apiUrl + city + '?key=DJN72HVKMNSNA7T9XK7F4FT9E')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data, "data");
+
+        document.querySelector(".city").innerHTML = data.resolvedAddress;
+        document.querySelector(".temp").innerHTML = Math.round((data.currentConditions.temp - 30) / 2) + "&#8451;";
+        document.querySelector(".humidity").innerHTML = data.currentConditions.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.currentConditions.windspeed + " km/h";
+
+      
+        switch (data.currentConditions.conditions) {
+          case 'Partially cloudy':
+          case 'Overcast':
+              weatherIcon.className = "fa-sharp fa-regular fa-cloud fa-3x";
+              break;
+          case 'Rain':
+              weatherIcon.className = "fa-solid fa-cloud-showers";
+              break;
+          case 'Clear':
+              weatherIcon.className = "fa-regular fa-sun fa-3x";
+              break;
+          default:
+            
+              break;
+      }
+    })
+    
+
   }
 
   function toggleMenu() {
