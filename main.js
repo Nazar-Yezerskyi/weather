23a53a2a410d2d488d502d6a3eaed9da7fb6b869
Назар Єзерський <nazar@Nazars-MacBook-Pro.local>
2023-07-14 16:10:48 +0300

    #3
    following your instructions corrected the flaws

--- main.js
+++ main.js
@@ -1,94 +1,148 @@
-function initialize() {
-  const apiKey = 'df59189ce5d7f9c060464802b2848b6f';
-  const apiUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
-  const searchInput = document.querySelector('.search input');
-  const searchButton = document.querySelector('.search button');
-  const menu = document.querySelector('.menu');
-  const modal = document.querySelector('.q1');
-  const buttonElem = document.querySelector('.modal__main button');
-  const html = `
-        <div class="menu">
-            <div class="weather">
-                <div class="weather-icon">
-                    <i class="fa-regular fa-sun fa-3x"></i>
-                </div>
-                <h1 class="temp">22 &#8451;</h1>
-                <h1 class="city">Lviv</h1>
-            </div>
-            <div class="detail">
-                <div class="details">
-                    <i class="fa-regular fa-droplet fa-3x"></i>
-                    <h1 class="humidity">50%</h1>
-                </div>
-                <div class="details">
-                    <i class="fa-regular fa-wind fa-3x"></i>
-                    <h1 class="wind">23 km/h</h1>
-                </div>
-            </div>
-        </div>
-  `;
-   menu.innerHTML = html;
-   const weatherIcon = document.querySelector('.weather-icon i');
+function createMenu() {
+  const menu = document.createElement('div');
+  menu.classList.add('menu');
 
-  function checkWeather(city) {
-    fetch(apiUrl + city + '?key=DJN72HVKMNSNA7T9XK7F4FT9E')
+  const weatherDiv = document.createElement('div');
+  weatherDiv.classList.add('weather');
+
+  const weatherIconDiv = document.createElement('div');
+  weatherIconDiv.classList.add('weather-icon');
+
+  const weatherIcon = document.createElement('i');
+  weatherIcon.classList.add('fa-regular', 'fa-sun', 'fa-3x');
+
+  weatherIconDiv.appendChild(weatherIcon);
+
+  const tempHeading = document.createElement('h1');
+  tempHeading.setAttribute('id','temp');
+  tempHeading.innerHTML = '22 &#8451;';
+
+  const cityHeading = document.createElement('h1');
+  cityHeading.setAttribute('id','city');
+  cityHeading.innerHTML = 'Lviv';
+
+  weatherDiv.appendChild(weatherIconDiv);
+  weatherDiv.appendChild(tempHeading);
+  weatherDiv.appendChild(cityHeading);
+
+  const detailDiv = document.createElement('div');
+  detailDiv.classList.add('detail');
+
+  const detailsDiv1 = document.createElement('div');
+  detailsDiv1.classList.add('details');
+
+  const dropletIcon = document.createElement('i');
+  dropletIcon.classList.add('fa-regular', 'fa-droplet', 'fa-3x');
+
+  const humidityHeading = document.createElement('h1');
+  humidityHeading.setAttribute('id', 'humidity');
+  humidityHeading.innerHTML = '50%';
+
+  detailsDiv1.appendChild(dropletIcon);
+  detailsDiv1.appendChild(humidityHeading);
+
+  const detailsDiv2 = document.createElement('div');
+  detailsDiv2.classList.add('details');
+
+  const windIcon = document.createElement('i');
+  windIcon.classList.add('fa-regular', 'fa-wind', 'fa-3x');
+
+  const windHeading = document.createElement('h1');
+  windHeading.setAttribute('id','wind');
+  windHeading.innerHTML = '23 km/h';
+
+  detailsDiv2.appendChild(windIcon);
+  detailsDiv2.appendChild(windHeading);
+
+  detailDiv.appendChild(detailsDiv1);
+  detailDiv.appendChild(detailsDiv2);
+
+  menu.appendChild(weatherDiv);
+  menu.appendChild(detailDiv);
+
+  return menu;
+}
+
+
+function checkWeather(apiUrl, city) {
+  return fetch(apiUrl + city + '?key=DJN72HVKMNSNA7T9XK7F4FT9E')
     .then(response => response.json())
     .then(data => {
-        console.log(data, "data");
+      console.log(data, "data");
 
-        document.querySelector(".city").innerHTML = data.resolvedAddress;
-        document.querySelector(".temp").innerHTML = Math.round((data.currentConditions.temp - 30) / 2) + "&#8451;";
-        document.querySelector(".humidity").innerHTML = data.currentConditions.humidity + "%";
-        document.querySelector(".wind").innerHTML = data.currentConditions.windspeed + " km/h";
+      document.getElementById("city").innerHTML = data.resolvedAddress;
+      document.getElementById("temp").innerHTML = Math.round((data.currentConditions.temp - 30) / 2) + "&#8451;";
+      document.getElementById("humidity").innerHTML = data.currentConditions.humidity + "%";
+      document.getElementById("wind").innerHTML = data.currentConditions.windspeed + " km/h";
 
-      
-        switch (data.currentConditions.conditions) {
-          case 'Partially cloudy':
-          case 'Overcast':
-              weatherIcon.className = "fa-sharp fa-regular fa-cloud fa-3x";
-              break;
-          case 'Rain':
-              weatherIcon.className = "fa-solid fa-cloud-showers";
-              break;
-          case 'Clear':
-              weatherIcon.className = "fa-regular fa-sun fa-3x";
-              break;
-          default:
-            
-              break;
-      }
-    })
-    
+      const weatherIcon = document.querySelector('.weather-icon i');
+      setWeatherIcon(weatherIcon, data.currentConditions.conditions);
+    });
+}
 
+function setWeatherIcon(weatherIcon, conditions) {
+  switch (conditions) {
+    case 'Partially cloudy':
+    case 'Overcast':
+      weatherIcon.className = "fa-sharp fa-regular fa-cloud fa-3x";
+      break;
+    case 'Rain':
+      weatherIcon.className = "fa-solid fa-cloud-showers";
+      break;
+    case 'Clear':
+      weatherIcon.className = "fa-regular fa-sun fa-3x";
+      break;
+    default:
+      break;
   }
+}
 
-  function toggleMenu() {
-    if (!menu.classList.contains('menu_active')) {
-      menu.classList.add('menu_active');
-      checkWeather(searchInput.value);
-    }
-    checkWeather(searchInput.value);
+
+function toggleMenu(apiUrl) {
+  const menu = document.querySelector('.menu');
+  const searchInput = document.querySelector('.search input');
+  if (!menu.classList.contains('menu_active')) {
+    menu.classList.add('menu_active');
+    checkWeather(apiUrl, searchInput.value);
   }
+  checkWeather(apiUrl, searchInput.value);
+}
 
-  function openModal() {
-    modal.classList.add('active');
-    setTimeout(closeModal, 3000);
-  }
+function openModal() {
+  const modal = document.querySelector('.modal_box');
+  modal.classList.add('active');
+  setTimeout(closeModal, 3000);
+}
 
-  function closeModal() {
-    modal.classList.remove('active');
-  }
+function closeModal() {
+  const modal = document.querySelector('.modal_box');
+  modal.classList.remove('active');
+}
 
-  buttonElem.addEventListener('click', closeModal);
+function initialize() {
+  const apiKey = 'df59189ce5d7f9c060464802b2848b6f';
+  const searchButton = document.querySelector('.search button');
+  const buttonElem = document.querySelector('.modal__main button');
+  const menu = createMenu();
+  document.body.appendChild(menu);
 
-  searchButton.addEventListener('click', function () {
+  function searchButtonClick() {
+    const searchInput = document.querySelector('.search input');
+    const apiUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
     if (searchInput.value.trim() === '') {
       openModal();
     } else {
-      toggleMenu();
+      toggleMenu(apiUrl);
     }
     searchInput.value = '';
-  });
+  }
+
+  function closeButtonClick() {
+    closeModal();
+  }
+
+  buttonElem.addEventListener('click', closeButtonClick);
+  searchButton.addEventListener('click', searchButtonClick);
 }
 
 document.addEventListener('DOMContentLoaded', initialize);
